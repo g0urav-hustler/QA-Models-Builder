b6e4a49ae45989177fd387491c464e9b8d0c36cc
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-13 15:53:03 +0530

    created main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.BERT import logger
+from src.BERT.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
