6d1cf5ee270cd77e1a9a0cf56587fcfafc6cf877
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-15 13:40:49 +0530

    add data preprocessing pipeling into main file

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from src.BERT import logger
 from src.BERT.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from src.BERT.pipeline.stage_02_data_preprocessing import DataPreprocessingPipeline                                                    
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Preprocessing Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataPreprocessingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
