f70c1e0d655bbef1379012420d562a1ae36eea5f
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-15 13:24:38 +0530

    update data processing entity

--- src/BERT/entity/config_entity.py
+++ src/BERT/entity/config_entity.py
@@ -1,8 +1,24 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-# creating a data ingesion config class
+# data ingestion class
 @dataclass(frozen=True)
 class DataIngestionConfig:
     raw_data_dir : Path
     web_data_dir : Path
+
+
+# data preprocessing class
+@dataclass(frozen= True)
+class DataProcessing:
+    raw_data_dir: Path
+    processed_data_dir : Path
+    split_data_dir : Path
+
+    context_col: str
+    question_col : str
+    answer_col : str
+    answer_start_col : str
+
+    train_data_size: float
+    val_data_size: float
