c5dd1e248bdcd98f0e3778088f36fe61cab5a18f
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-16 16:35:10 +0530

    adding train model pipeline into main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.BERT import logger
 from src.BERT.pipeline.stage_01_data_ingestion import DataIngestionPipeline
-from src.BERT.pipeline.stage_02_data_preprocessing import DataPreprocessingPipeline                                                    
+from src.BERT.pipeline.stage_02_data_preprocessing import DataPreprocessingPipeline    
+from src.BERT.pipeline.stage_03_train_model import TrainModelPipeline                                                
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -19,6 +20,18 @@ STAGE_NAME = "Data Preprocessing Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataPreprocessingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Train Model Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = TrainModelPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
