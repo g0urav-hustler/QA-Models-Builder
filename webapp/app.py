7d8a73c60eeab95a68d3653541701d321e10a69f
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-17 15:09:55 +0530

    update web application

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import yaml
 
 # page config
 st.set_page_config(page_title="QA-Model-Builder ", layout="wide",)
@@ -13,9 +14,31 @@ st.markdown("This application helps you to build your own question answer model 
 uploaded_file = st.file_uploader("Upload your question answer file in csv format")
 
 if uploaded_file is not None:
-    folder = 'uploaded_files'
-    with open(os.path.join(folder, "version1.csv"), "wb") as f:
+    folder = 'web_files'
+    with open(os.path.join(folder, "data.csv"), "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-print(uploaded_file)
-print('Type --', type(uploaded_file))
+
+yaml_data_format = {
+    "data_processing":
+        {
+            "context_col": context_column,
+            "question_col": question_column,
+            "anwer_col": answer_column,
+            "answer_start_col": answer_start_column,
+
+            "train_data_size": train_data_size,
+            "val_data_size": val_data_size,
+
+        },
+    "model_params":
+        {
+            "model_type": model_type,
+            "model_name": model_name,
+            "epochs": no_of_epochs,
+            "train_batch_size": train_batch_size,
+            "val_batch_size": val_batch_size
+        }
+
+}
+
