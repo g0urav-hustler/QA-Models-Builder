28bdbf4df46d5334fde5011a11d9ba66c2e6d78e
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-18 12:58:33 +0530

    update main pipeline

--- main.py
+++ main.py
@@ -3,37 +3,52 @@ from src.BERT.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from src.BERT.pipeline.stage_02_data_preprocessing import DataPreprocessingPipeline    
 from src.BERT.pipeline.stage_03_train_model import TrainModelPipeline                                                
 
-STAGE_NAME = "Data Ingestion Stage"
 
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    logger.exception(e)
-    raise e
+class InvokePipeline:
+    def __init__(self):
+        pass
 
+    def main(self):
 
-STAGE_NAME = "Data Preprocessing Stage"
+        STAGE_NAME = "Data Ingestion Stage"
 
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataPreprocessingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    logger.exception(e)
-    raise e
+        try:
+            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+            obj = DataIngestionPipeline()
+            obj.main()
+            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        except Exception as e:
+            logger.exception(e)
+            raise e
 
 
-STAGE_NAME = "Train Model Stage"
+        STAGE_NAME = "Data Preprocessing Stage"
 
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = TrainModelPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    logger.exception(e)
-    raise e
+        try:
+            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+            obj = DataPreprocessingPipeline()
+            obj.main()
+            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
+
+        STAGE_NAME = "Train Model Stage"
+
+        try:
+            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+            obj = TrainModelPipeline()
+            obj.main()
+            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        except Exception as e:
+            logger.exception(e)
+            raise e
+        
+if __name__ == '__main__':
+    try:
+        obj = InvokePipeline()
+        obj.main()
+    except Exception as e:
+        logger.exception(e)
+        raise e
